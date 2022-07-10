72c9b38cdeed3c9504bf38bb00524a4eb15cd72a
Cody Greenwalt <102393920+cgreenwalt73@users.noreply.github.com>
2022-07-09 22:57:55 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+      <div className='list'>
+        <ul>
+          <li>Finish MERN Stack</li>
+          <li>Network</li>
+          <li>Read for bookclub</li>
+          <li>Take Seven for more walks</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
